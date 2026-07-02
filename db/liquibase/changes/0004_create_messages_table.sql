ff2f2d81d3cd9d15f45e1b64573e769d6ceb6d09
m-nishide-sr <292954160+m-nishide-sr@users.noreply.github.com>
2026-07-02 09:38:19 +0900

    Copilotの作成したTODO事項の解決

--- db/liquibase/changes/0004_create_messages_table.sql
+++ db/liquibase/changes/0004_create_messages_table.sql
@@ -3,7 +3,7 @@
 CREATE TABLE public.messages (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   user_name text NOT NULL DEFAULT '',
-  cognito_id uuid NOT NULL,
+  cognito_id uuid NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000',
   created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   body text NOT NULL DEFAULT '',
   row_log text NOT NULL,
